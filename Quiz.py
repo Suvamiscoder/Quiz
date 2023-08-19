e31c9458f101d04dce05eadb97cf053f8c626b2e
Suvamiscoder <141615744+Suvamiscoder@users.noreply.github.com>
2023-08-19 16:22:20 +0545

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -2,28 +2,40 @@ print('Welcome to my Quiz Game')
 playing= input("Do You want to play?  ")
 if playing !='yes':
     quit()
-print('Okay! Lets play 🙂')
+print('Okay! Lets play :)')
 answer=input('Who is the president of India?   ')
 
 if answer=="Droupadi Murmu":
     print('Correct Answer!')
 else:
     print("Wrong Answer!!   Try Again..")
-answer=input('Who is the president of India?   ')
+answer=input('Which country has the highest mountain in the world?')
 
-if answer=="Droupadi Murmu":
+if answer=="Nepal":
     print('Correct Answer!')
 else:
     print("Wrong Answer!!   Try Again..")
-answer=input('Who is the president of India?   ')
+answer=input('Which is the highest mountaion in the world?')
 
-if answer=="Droupadi Murmu":
+if answer=="Mount Everest":
     print('Correct Answer!')
 else:
     print("Wrong Answer!!   Try Again..")
-answer=input('Who is the president of India?   ')
+answer=input('who is the prime minister of India?')
 
-if answer=="Droupadi Murmu":
+if answer=="Narendra Modi":
+    print('Correct Answer!')
+else:
+    print("Wrong Answer!!   Try Again..")
+answer=input('who is the prime minister of India?')
+
+if answer=="Narendra Modi":
+    print('Correct Answer!')
+else:
+    print("Wrong Answer!!   Try Again..")
+answer=input('who is the prime minister of India?')
+
+if answer=="Narendra Modi":
     print('Correct Answer!')
 else:
     print("Wrong Answer!!   Try Again..")

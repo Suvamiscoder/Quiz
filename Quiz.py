4901224f3cea7c8590224289b14a7ee4162db42a
Diparsan79 <142617074+Diparsan79@users.noreply.github.com>
2023-08-19 16:20:58 +0545

    hello
    this is a quiz

---
+++ Quiz.py
@@ -0,0 +1,29 @@
+print('Welcome to my Quiz Game')
+playing= input("Do You want to play?  ")
+if playing !='yes':
+    quit()
+print('Okay! Lets play 🙂')
+answer=input('Who is the president of India?   ')
+
+if answer=="Droupadi Murmu":
+    print('Correct Answer!')
+else:
+    print("Wrong Answer!!   Try Again..")
+answer=input('Who is the president of India?   ')
+
+if answer=="Droupadi Murmu":
+    print('Correct Answer!')
+else:
+    print("Wrong Answer!!   Try Again..")
+answer=input('Who is the president of India?   ')
+
+if answer=="Droupadi Murmu":
+    print('Correct Answer!')
+else:
+    print("Wrong Answer!!   Try Again..")
+answer=input('Who is the president of India?   ')
+
+if answer=="Droupadi Murmu":
+    print('Correct Answer!')
+else:
+    print("Wrong Answer!!   Try Again..")
